f196a1ee7e5499c44c61882d13f9504644b24b15
Samiksha Kute <samikshakute2002@gmail.com>
2025-03-24 12:25:05 +0530

    Update Solution.java

--- Day 3/src/Solution.java
+++ Day 3/src/Solution.java
@@ -4,14 +4,11 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        if (n % 2 == 0) {
-            if (n <= 5 || n > 20) {
-                System.out.println("Not Weird");
-            } else {
-                System.out.println("Weird");
-            }
-        } else {
+        int N = in.nextInt();
+        if(N % 2 == 0 && (N < 6 || N > 20)) {
+            System.out.println("Not Weird");
+        }
+        else {
             System.out.println("Weird");
         }
     }
